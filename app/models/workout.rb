0a8698e7792a5b819108fd6a666301f4e271f6ec
leobr <leobrionesherrera@gmail.com>
2020-07-29 16:28:21 -0500

    Users can delete workouts

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,9 +1,9 @@
 class Workout < ApplicationRecord
-  has_many :scheduled_workouts
+  has_many :scheduled_workouts, dependent: :destroy
   has_many :users, through: :scheduled_workouts
-  has_many :exercise_workouts
-  has_many :exercises, through: :exercise_workouts
-  has_many :category_workouts
+  has_many :exercise_workouts, dependent: :destroy
+  has_many :exercises, through: :exercise_workouts, dependent: :destroy
+  has_many :category_workouts, dependent: :destroy
   has_many :categories, through: :category_workouts
 
   validates :name, presence: true
