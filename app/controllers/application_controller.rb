05f8b2d1a38b1e070c5603f5028470f49f980ee9
leobr <leobrionesherrera@gmail.com>
2020-07-29 14:31:19 -0500

    Added home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     include UsersHelper
+    
+    def home
+        if logged_in?
+            render :index
+        else
+            render :home
+        end
+    end
 end
