c52da2606c6b53735e66c1574d4740de19f3f6cf
leobr <leobrionesherrera@gmail.com>
2020-07-30 16:25:14 -0500

    Created helpers to display buttons

--- app/helpers/exercise_workouts_helper.rb
+++ app/helpers/exercise_workouts_helper.rb
@@ -1,2 +1,19 @@
 module ExerciseWorkoutsHelper
+    def display_edit_for_exercise(workout, exercise)
+        if current_user.created_workouts.include?(workout)
+            link_to 'Edit Exercise', edit_workout_exercise_path(@workout, exercise)
+        end
+    end
+
+    def display_delete_for_exercise(workout, exercise)
+        if current_user.created_workouts.include?(workout)
+            link_to 'Delete Exercise', [workout, exercise], method: :delete
+        end
+    end
+
+    def display_add_exersice(workout)
+        if current_user.created_workouts.include?(workout)
+            link_to 'Add Exercise', new_workout_exercise_path(workout)
+        end
+    end
 end
