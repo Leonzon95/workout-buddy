b28d5e51e1db336531caa83942759994f4250662
leobr <leobrionesherrera@gmail.com>
2020-07-30 16:46:34 -0500

    Added helpers

--- app/helpers/scheduled_workouts_helper.rb
+++ app/helpers/scheduled_workouts_helper.rb
@@ -1,10 +1,10 @@
 module ScheduledWorkoutsHelper
-    def display_todays_workout
+    def display_todays_workout 
         workouts = current_user.scheduled_workouts.where(start_time: DateTime.current.beginning_of_day)
         if !workouts.empty?
             render partial: 'show_workout', locals: {workouts: workouts}
         else
-            "You are resting today!"
+            tag.h1("You are resting today!")
         end
     end
 end
--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -5,15 +5,17 @@ module WorkoutsHelper
         end
     end
 
-    def display_edit(workout)
+    def display_edit_workout(workout)
         if current_user.created_workouts.include?(workout)
             link_to 'Edit Workout', edit_workout_path(workout)
         end
     end
 
-    def display_delete(workout)
+    def display_delete_workout(workout)
         if current_user.created_workouts.include?(workout)
             link_to 'Delete Workout', workout, method: :delete 
+        elsif current_user.workouts.include?(workout)
+            link_to 'Remove from Calendar', workout, method: :delete
         end
     end
 
